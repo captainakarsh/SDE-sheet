fd80b340bb5c5ba8430d060d752d39c1334922f6
Akarsh singh <81631252+captainakarsh@users.noreply.github.com>
2022-07-03 01:34:31 +0530

    Time: 0 ms (100.00%), Space: 6 MB (80.53%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,49 +1,24 @@
 class Solution {
 public:
-    void next(vector<int> &n)
-    {
-        int index=-1;
-        for(int i=n.size()-1;i>0;--i)
-        {
-            if(n[i-1]<n[i]){
-                index = i-1;
-                break;
-            }
-        }
-        if(index==-1)return;
-        for(int i=n.size()-1;i>index;--i)
-        {
-            if(n[i]>n[index])
-            {
-                int temp = n[index];
-                n[index] = n[i];
-                n[i] = temp;
-                break;
-            }
-        }
-        int i = index+1,j = n.size()-1;
-        while(i<j)
-        {
-            int temp = n[i];
-            n[i] = n[j];
-            n[j] = temp;
-            i++;j--;
-        }
-    }
+
     string getPermutation(int n, int k) {
-        vector<int> ans;
-        for(int i=1;i<=n;++i)
-        ans.push_back(i);k--;
-        while(k)
+        vector<int> num;
+        int fact = 1;
+        for(int i=1;i<n;++i)
         {
-            next(ans);
-            k--;
+            fact=fact*i;
+            num.push_back(i);
         }
-        string fans="";
-        for(int i=0;i<n;++i)
+        num.push_back(n);k--;
+        string  ans="";
+        while(true)
         {
-            fans=fans+ to_string(ans[i]);
+            ans=ans+to_string(num[k/fact]);
+            num.erase(num.begin()+k/fact);
+            if(num.size()==0)break;
+            k = k%fact;
+            fact = fact / num.size();
         }
-        return fans;
+        return ans;
     }
 };
